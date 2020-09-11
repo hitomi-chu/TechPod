6be6b84705501bb4dddfbb53aee7352b34ab8f7c
Hitomi Chu <hitomichutatematsu@keio.jp>
2020-09-12 03:31:33 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by Hitomi Chu on 12/9/2020.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
